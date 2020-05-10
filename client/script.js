cb685165bfbd467802ab885e8c43fd25944abdd3
tamim-khan <tamim.m.khan@gmail.com>
2020-05-10 15:46:31 -0400

    add pressure apple pencil support

--- client/script.js
+++ client/script.js
@@ -1,26 +1,43 @@
-const socket = io('http://localhost:3000');
+const socket = io(/* 'http://host:port/' */);
+var pressure = 0;
 
 function setup() {
+  disableScroll();
   createCanvas(displayWidth, displayHeight);
+  Pressure.set('canvas', {
+    change: (force, event) => {
+      pressure = force;
+    }
+  });
   background(255);
 
   socket.on('coord', (data) => {
-    drawEllipse(data.x, data.y, 150);
+    stroke(128);
+    strokeWeight(10 * data.pressure);
+    line(data.x, data.y, data.px, data.py);
   });
 }
 
 function mouseDragged() {
   let coord = {
     x: mouseX,
-    y: mouseY
+    y: mouseY,
+    px: pmouseX,
+    py: pmouseY,
+    pressure: pressure
   };
   socket.emit('coord', coord);
-
-  drawEllipse(mouseX, mouseY, 50);
+  stroke(50);
+  strokeWeight(10 * pressure);
+  line(mouseX, mouseY, pmouseX, pmouseY);
 }
 
-function drawEllipse(x, y, color) {
-  noStroke();
-  fill(color);
-  ellipse(x, y, 10, 10);
+function disableScroll() {
+  document.body.addEventListener(
+    'touchmove',
+    (e) => {e.preventDefault()},
+    { 
+      passive: false
+    }
+  );
 }
